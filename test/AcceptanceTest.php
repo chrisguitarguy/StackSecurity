f57ca060f28abbbae3f8197b093f2a20ca6776f0
Christopher Davis <cdavis9999@gmail.com>
2015-03-01 14:31:50 -0500

    Expand the Acceptance Test to Use All Kernels

--- test/AcceptanceTest.php
+++ test/AcceptanceTest.php
@@ -7,9 +7,16 @@
  * @copyright   (c) Christopher Davis <http://christopherdavis.me>
  */
 
-namespace Chrisguitarguy\StackSecurity\Authentication;
+namespace Chrisguitarguy\StackSecurity;
 
 use Stack\CallableHttpKernel;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\RequestMatcher;
+use Symfony\Component\HttpKernel\Client;
+use Symfony\Component\HttpKernel\HttpKernelInterface;
+use Symfony\Component\Security\Core\Authentication\Token\AnonymousToken;
+use Symfony\Component\Security\Core\Authorization\AccessDecisionManager;
+use Symfony\Component\Security\Core\Authorization\Voter\RoleVoter;
 use Symfony\Component\Security\Core\Authentication\AuthenticationProviderManager;
 use Symfony\Component\Security\Core\Authentication\Provider\SimpleAuthenticationProvider;
 use Symfony\Component\Security\Core\Encoder\EncoderFactory;
@@ -17,18 +24,17 @@ use Symfony\Component\Security\Core\Encoder\PlainTextPasswordEncoder;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoder;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Security\Core\User\InMemoryUserProvider;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpKernel\Client;
-use Symfony\Component\HttpKernel\HttpKernelInterface;
+use Symfony\Component\Security\Http\AccessMap;
 
 /**
- * Tests all the authentication kernels together.
+ * Test all of the authentication/authorization kernels together.
  *
  * @group acceptance
  */
-class AuthenticationAcceptanceTest extends \Chrisguitarguy\StackSecurity\TestCase
+class AcceptanceTest extends \Chrisguitarguy\StackSecurity\TestCase
 {
     const USER = 'testUser';
+    const USER2 = 'testUser2';
     const PASS = 'password';
     const PROVIDERKEY = 'test';
 
@@ -81,7 +87,25 @@ class AuthenticationAcceptanceTest extends \Chrisguitarguy\StackSecurity\TestCas
         $this->assertEquals('Basic realm="TestRealm"', $resp->headers->get('WWW-Authenticate'));
     }
 
-    public function testRequestToFirewalledAreaWithCorrectUserAndPasswordGetsResponse()
+    /**
+     * @expectedException Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException
+     */
+    public function testRequestToFirewalledAreaWithUserLackingRolesIsDeniedAccess()
+    {
+        $app = $this->createApplication();
+        $client = $this->createClient($app, [
+            'PHP_AUTH_USER' => self::USER2,
+            'PHP_AUTH_PW'   => self::PASS,
+        ]);
+
+        $client->request('GET', '/firewalled');
+        $resp = $client->getResponse();
+
+        $this->assertTrue($resp->isOk());
+        $this->assertEquals('Hello, World', $resp->getContent());
+    }
+
+    public function testRequestToFirewalledAreaWithCorrectUserPasswordAndGetsResponse()
     {
         $app = $this->createApplication();
         $client = $this->createClient($app, [
@@ -110,40 +134,53 @@ class AuthenticationAcceptanceTest extends \Chrisguitarguy\StackSecurity\TestCas
         $this->assertEquals('Basic realm="TestRealm"', $resp->headers->get('WWW-Authenticate'));
     }
 
+    public function testRequestToBlockedAreaWithoutAuthenticationIssuesChallenge()
+    {
+        $app = $this->createApplication();
+        $client = $this->createClient($app);
+
+        $client->request('GET', '/blocked');
+        $resp = $client->getResponse();
+
+        $this->assertEquals(401, $resp->getStatusCode());
+        $this->assertEquals('Basic realm="TestRealm"', $resp->headers->get('WWW-Authenticate'));
+    }
+
     private function createApplication(HttpKernelInterface $app=null)
     {
         $app = $app ?: $this->kernelReturning(new Response('Hello, World'));
+
+        $accessMap = new AccessMap();
+        $accessMap->add(new RequestMatcher('^/firewalled'), ['ROLE_USER']);
+        $accessMap->add(new RequestMatcher('^/blocked'), ['ROLE_ADMIN']);
+        $decisionManager = new AccessDecisionManager([
+            new RoleVoter(),
+        ]);
+
         $authenticator = $this->createAuthenticator();
 
         $userProvider = new InMemoryUserProvider([
             self::USER  => ['password' => self::PASS, 'roles' => ['ROLE_USER']],
+            self::USER2  => ['password' => self::PASS, 'roles' => []],
         ]);
 
         $authManager = new AuthenticationProviderManager([
             new SimpleAuthenticationProvider($authenticator, $userProvider, self::PROVIDERKEY),
         ]);
 
-        $firewall = new Firewall\ChainFirewall([
-            new Firewall\PreAuthFirewall('^/firewalled', self::PROVIDERKEY, $authenticator),
+        $firewall = new Authentication\Firewall\ChainFirewall([
+            new Authentication\Firewall\PreAuthFirewall('^/firewalled', self::PROVIDERKEY, $authenticator),
         ]);
 
         $app = (new \Stack\Builder())
-            ->push(ChallengingKernel::class, $authenticator)
-            ->push(ErrorHandlingKernel::class, $authenticator)
-            ->push(FirewallKernel::class, $firewall, $authManager)
+            ->push(Authentication\ChallengingKernel::class, $authenticator)
+            ->push(Authentication\ErrorHandlingKernel::class, $authenticator)
+            ->push(Authentication\FirewallKernel::class, $firewall, $authManager)
+            ->push(Authorization\ErrorHandlingKernel::class)
+            ->push(Authorization\AccessCheckingKernel::class, $decisionManager, $accessMap)
             ->resolve($app);
 
         return $app;
-    }
-
-    private function createAuthenticator()
-    {
-        $encoder = new UserPasswordEncoder(new EncoderFactory([
-            // don't do this in real life
-            UserInterface::class    => new PlainTextPasswordEncoder(),
-        ]));
-
-        return new Authenticator\HttpBasicAuthenticator('TestRealm', $encoder);
     }
 
     private function createClient(HttpKernelInterface $app, array $server=[])
@@ -156,5 +193,14 @@ class AuthenticationAcceptanceTest extends \Chrisguitarguy\StackSecurity\TestCas
         return new CallableHttpKernel(function () use ($resp) {
             return $resp;
         });
+    }
+
+    private function createAuthenticator()
+    {
+        $encoder = new UserPasswordEncoder(new EncoderFactory([
+            UserInterface::class    => new PlainTextPasswordEncoder(),
+        ]));
+
+        return new Authentication\Authenticator\HttpBasicAuthenticator('TestRealm', $encoder);
     }
 }
